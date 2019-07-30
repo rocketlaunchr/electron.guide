cdd8ab8b4b4bcce60eb8522c4330dfb04bd4fbd2
rocketlaunchr-cto <rocketlaunchr.cloud@gmail.com>
2019-07-30 23:45:45 +1000

    - build site

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,15 +1,29 @@
-$(document).ready(function(){
-    $(".doc-sidebar-list__item__children-toggle--show").click(function(e){
-        e.preventDefault();
-        $(this).attr("href","javascript:void(0)");
-        $(this).toggleClass("doc-sidebar-list__item__children-toggle doc-sidebar-list__item__children-toggle--hide");
-        $(this).toggleClass("doc-sidebar-list__item__children-toggle doc-sidebar-list__item__children-toggle--show");
-        
-        $(this).parent('li').toggleClass('doc-sidebar-list__item doc-sidebar-list__item--link doc-sidebar-list__item--current doc-sidebar-list__item--has-children');
-        $(this).parent('li').toggleClass('doc-sidebar-list__item doc-sidebar-list__item--link doc-sidebar-list__item--has-children doc-sidebar-list__item--children-list--hidden');
+$(document).ready(function() {
+  $(".doc-sidebar-list__item__children-toggle--show").click(function(e) {
+    e.preventDefault();
+    $(this).attr("href", "javascript:void(0)");
+    $(this).toggleClass(
+      "doc-sidebar-list__item__children-toggle doc-sidebar-list__item__children-toggle--hide"
+    );
+    $(this).toggleClass(
+      "doc-sidebar-list__item__children-toggle doc-sidebar-list__item__children-toggle--show"
+    );
 
-        $(this.nextElementSibling).toggleClass('doc-sidebar-list__children-list');
-        $(this.nextElementSibling).toggleClass('doc-sidebar-list__children-list doc-sidebar-list__children-list--hidden');
-        console.log("anchor open");
-    });
+    $(this)
+      .parent("li")
+      .toggleClass(
+        "doc-sidebar-list__item doc-sidebar-list__item--link doc-sidebar-list__item--current doc-sidebar-list__item--has-children"
+      );
+    $(this)
+      .parent("li")
+      .toggleClass(
+        "doc-sidebar-list__item doc-sidebar-list__item--link doc-sidebar-list__item--has-children doc-sidebar-list__item--children-list--hidden"
+      );
+
+    $(this.nextElementSibling).toggleClass("doc-sidebar-list__children-list");
+    $(this.nextElementSibling).toggleClass(
+      "doc-sidebar-list__children-list doc-sidebar-list__children-list--hidden"
+    );
+    console.log("anchor open");
+  });
 });
