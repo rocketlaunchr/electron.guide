2ab02d34d2392fa9c3e1cc9e32fdbd98c3701e9d
VictorAlagwu <victoralagwu@gmail.com>
2019-08-01 01:20:37 +0100

    --Rendering HTML on sidebar

--- themes/hexohugoincubator/static/js/script.js
+++ themes/hexohugoincubator/static/js/script.js
@@ -26,9 +26,7 @@ $(document).ready(function() {
     );
     console.log("anchor open");
   });
-});
 
-$(document).ready(function() {
   $("a span:contains(<strong>)").each(function() {
     $(this).html( $(this).text() );
   })
